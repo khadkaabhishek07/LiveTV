d2fe8333274e4ef8be79821ad3ebeec87eb004c7
Abhishek Khadka <abhishekkhadka@Abhisheks-MacBook-Air.local>
2026-05-06 01:32:39 +0545

    cors added

--- app/config/settings.py
+++ app/config/settings.py
@@ -154,10 +154,12 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://192.168.18.26:8080',
+    'https://sittaimatv.onrender.com',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://192.168.18.26:8080',
+    'https://sittaimatv.onrender.com',
 ]
